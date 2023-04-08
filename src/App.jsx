8da86215caf522e73d89dbf46e4b2643c32d77af
Xcode-X <victorjames40v@gmail.com>
2023-04-08 16:08:09 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App bg-hsl(0, 0%, 100%) relative">
+    <div className="App bg-hsl(0, 0%, 100%) relative overflow-x-hidden">
       <NavBar/>
       <div>
         <FirstPage/>
--- src/components/pages/firstPage.jsx
+++ src/components/pages/firstPage.jsx
@@ -3,25 +3,27 @@ import images from "../assets/image-mockups.png"
  function FirstPage(){
     return(
         <div className="w-[100%] h-[90vh] flex flex-row">
-            <div className="pt-[15%] pl-[40px]">
-                <div className="w-[60%]">
+            <div className="pt-[15%] pl-[40px] w-[50%]">
+                <div className="w-[100%]"> 
                     <h2 className="text-[50px]">Next generation digital banking</h2>
                     <p className="py-[20px]">Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</p>
                 </div>
 
                 <div>
-                    <button className="bg-Lime_Green p-[10px] rounded-[10%]">
+                    <button className="bg-Lime_Green p-[10px] rounded-[20%]">
                         Request invite
                     </button>
                 </div>
                 
             </div>
-            <div className="bg-bg_desktop bg-center bg-cover">
-                <img
-                className=" "
-                 src={images}
-                 alt="image" 
-                />
+            <div className="bg-bg_desktop bg-center bg-cover w-[90%]">
+                <div className="ml-[20%] w-[100%]">
+                    <img
+                    className=""
+                    src={images}
+                    alt="image" 
+                    />
+                </div>
             </div>
         </div>
     )
